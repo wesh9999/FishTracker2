0f1f2725220b98fd4336971c29d3ccf152e1b9d5
wesh9999 <wesh9999@gmail.com>
2023-06-09 11:23:36 -0400

    Updated project settings to get it working with latest IntelliJ

--- app/src/androidTest/java/org/weshley/fishtracker2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/weshley/fishtracker2/ExampleInstrumentedTest.java
@@ -15,11 +15,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.weshley.fishtracker2", appContext.getPackageName());
-    }
+public class ExampleInstrumentedTest
+{
+   @Test
+   public void useAppContext()
+   {
+      // Context of the app under test.
+      Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+      assertEquals("org.weshley.fishtracker2", appContext.getPackageName());
+   }
 }
